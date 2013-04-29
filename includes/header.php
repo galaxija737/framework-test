614d4038dc0cc735a014810b20150f3e99697688
Nenad <galaxija.737@gmail.com>
2013-04-29 19:22:19 +0200

    Start of project

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<?php include('variables/variables.php'); ?>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="description" content="" />
+
+<meta name="keywords" content="" />
+
+<meta name="author" content="" />
+
+<link rel="stylesheet" type="text/css" href="normalize.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+
+<title>1stWebDesigner PHPa Template</title>
+
+</head>
+
+	<body>
+
+		<div id="wrapper">
+<div id="header">
+
+	<h2><?php echo $heading ?></h2>
+
+</div> <!-- end #header -->
